69d3d3f1fa0c78550745301aee08244fde89a17a
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-15 10:00:55 -0400

    add threshhold setting

--- TextToSpeechASDRTest.groovy
+++ TextToSpeechASDRTest.groovy
@@ -3,6 +3,7 @@ import javafx.scene.control.Alert
 import javafx.scene.control.Alert.AlertType
 import com.neuronrobotics.bowlerstudio.AudioPlayer
 import com.neuronrobotics.bowlerstudio.AudioStatus
+import com.neuronrobotics.bowlerstudio.IAudioProcessingLambda
 import com.neuronrobotics.bowlerstudio.ISpeakingProgress
 
 // code here
@@ -18,16 +19,57 @@ Platform.runLater( {
 
 while(a==null)
 	Thread.sleep(100);
+AudioPlayer.setThreshhold(600/65535.0)
+AudioPlayer.setLowerThreshhold(100/65535.0)
+AudioPlayer.setIntegralGain(1);
+AudioPlayer.setDerivitiveGain(1);
+AudioPlayer.setLambda( new IAudioProcessingLambda() {
+	
+	@Override
+	public AudioStatus update(AudioStatus currentStatus, double amplitudeUnitVector, double currentRollingAverage,
+			double currentDerivitiveTerm) {
+		switch(currentStatus) {
+		case AudioStatus.attack:
+			if(amplitudeUnitVector>AudioPlayer.getThreshhold()) {
+				currentStatus=AudioStatus.sustain;
+			}
+			break;
+		case AudioStatus.decay:
+			if(amplitudeUnitVector<AudioPlayer.getLowerThreshhold()) {
+				currentStatus=AudioStatus.release;
+			}
+			break;
+		case AudioStatus.release:
+			if(amplitudeUnitVector>AudioPlayer.getThreshhold()) {
+				currentStatus=AudioStatus.attack;
+			}
+			break;
+		case AudioStatus.sustain:
+			if(amplitudeUnitVector<AudioPlayer.getLowerThreshhold()) {
+				currentStatus=AudioStatus.decay;
+			}
+			break;
+		default:
+			break;
+		}
+		return currentStatus;
+	}
+	
+	@Override
+	public void startProcessing() {
+
+	}
+});
 
 ISpeakingProgress sp ={double percent,AudioStatus status->
 	println "Progress: "+percent+"% Status "+status+" "
 	if(a!=null) {
 		Platform.runLater( {
-			a.setContentText((status==AudioStatus.attack)?"0":"-");
+			a.setContentText((status==AudioStatus.attack||status==AudioStatus.sustain)?"0":"-");
 		});
 	}
 }
-BowlerKernel.speak("A test phrase... a pause...a quick, brown, fox jumpes over the lazy dog.", 100, 0, 201, 1.0, 1.0,sp)
+BowlerKernel.speak("A test phrase... a pause...a quick, brown fox jumpes over the lazy dog.", 100, 0, 201, 1.0, 1.0,sp)
 Platform.runLater( {a.close();})
 
 
