b908a5e46293ef3ec3196b2c4191c3072294d8a7
Kevin Harrington <mad.hephaestus@gmail.com>
2023-06-15 12:02:43 -0400

    simplified text to speech with lips

--- TextToSpeechASDRTest.groovy
+++ TextToSpeechASDRTest.groovy
@@ -1,6 +1,8 @@
+
 import com.neuronrobotics.bowlerstudio.AudioStatus
 import com.neuronrobotics.bowlerstudio.BowlerKernel
 import com.neuronrobotics.bowlerstudio.BowlerStudio
+import com.neuronrobotics.bowlerstudio.BowlerStudioController
 import com.neuronrobotics.bowlerstudio.ISpeakingProgress
 import com.neuronrobotics.bowlerstudio.creature.MobileBaseCadManager
 import com.neuronrobotics.bowlerstudio.creature.MobileBaseLoader
@@ -11,25 +13,38 @@ import com.neuronrobotics.sdk.addons.kinematics.MobileBase
 import com.neuronrobotics.sdk.common.DeviceManager
 import com.neuronrobotics.sdk.util.ThreadUtil
 
+import javafx.scene.control.Tab
 import javafx.scene.image.Image
+import javafx.application.Platform
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 HashMap<AudioStatus,Image> images = new HashMap<>()
-
+String url = "https://github.com/madhephaestus/TextToSpeechASDRTest.git"
 for(AudioStatus s:EnumSet.allOf(AudioStatus.class)) {
-	File f = new File(ScriptingEngine.fileFromGistID("https://github.com/madhephaestus/TextToSpeechASDRTest.git", "img/lisa-"+s.parsed+".png"))
-	Image image = new Image(f.getAbsolutePath());
+	File f = new File(ScriptingEngine.getRepositoryCloneDirectory(url).getAbsolutePath()+ "/img/lisa-"+s.parsed+".png")
+	println "Loading "+f.getAbsolutePath()
+	Image image = new Image(new FileInputStream(f.getAbsolutePath()));
 	images.put(s, image)
 }
+ImageView imageView = new ImageView(images.get(AudioStatus.X_NO_SOUND));
 
 ISpeakingProgress sp ={double percent,AudioStatus status->
-	println percent+" " +status
-	
+	//println percent+" " +status
+	Platform.runLater({
+		imageView.setImage(images.get(status))
+	})
 }
 
-println images
+Tab t = new Tab()
+t.setContent(imageView)
+BowlerStudioController.addObject(t, null)
+Thread.sleep(1000)
 
-
-double i=1
+double i=800
 try {
 	BowlerKernel.speak("The mighty Zoltar sees your future! You have much to look forward to!", 100, 0, i, 1.0, 1.0,sp)
 }catch(Throwable tr) {
